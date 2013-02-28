b9cd606810b31d897635991ebe5cb737c2495321
ryanhinkel <ryanhinkel@gmail.com>
2013-02-27 22:34:08 -0500

    new punctuation

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Dustin!?'
+    return 'Hello Dustin!!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
