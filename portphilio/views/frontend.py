bddcc7693680722ef9133e2952d64ba6823f9d78
Dustin Ingram <dustin.ingram@gmail.com>
2013-03-27 13:09:03 -0400

    Better, faster static frontend, also handles extensions correctly now

--- portphilio/views/frontend.py
+++ portphilio/views/frontend.py
@@ -6,12 +6,20 @@ mod = Blueprint('frontend', __name__)
 @mod.route('/', defaults={'path': ''})
 @mod.route('/<path:path>')
 def hello(path):
-    for p in [path, index(path), index(path + '/')] :
-        try :
-            return send_file(p)
-        except IOError:
-            pass
-    abort(404)
+    try :
+        if path.startswith('static') :
+            return send_file(path)
+        elif '.' in path.split('/')[-1] :
+            return send_file(domain(path))
+        else :
+            return send_file(index(path))
+    except IOError:
+        abort(404)
 
 def index(path):
+    if not path.endswith('/'):
+        path += '/'
     return 'static/localhost/' + path + 'index.html'
+
+def domain(path):
+    return 'static/localhost/' + path
