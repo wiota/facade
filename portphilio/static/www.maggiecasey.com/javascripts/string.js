b11ce83fc98f7bd82108d98e37d8582bb9e73756
Dustin Ingram <dustin.ingram@gmail.com>
2013-03-28 20:56:59 -0400

    Starting to duplicate Maggie's site

---
+++ portphilio/static/www.maggiecasey.com/javascripts/string.js
@@ -0,0 +1,5 @@
+var flashvars = {};
+var params = {};
+var attributes = {};
+attributes.id = "string";
+swfobject.embedSWF("../flash/string_index_cleaned_name.swf", "string", "1024", "239", "9.0.0", false, flashvars, params, attributes);
