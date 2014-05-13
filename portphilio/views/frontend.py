a7d39cd6d5c16bb8862addf522107515d3abcf0d
Dustin Ingram <dustin.ingram@gmail.com>
2014-05-13 10:47:56 -0400

    Root index is now always a rendered template

--- portphilio/views/frontend.py
+++ portphilio/views/frontend.py
@@ -5,7 +5,11 @@ import os
 
 mod = Blueprint('frontend', __name__)
 
-def index(path):
+# Create a path to a named template with the host name
+def template_path(template_name) :
+    return '/'.join([app.config['HOST'], template_name])
+
+def static_index(path):
     return '/'.join([domain(path), app.config['DIRECTORY_INDEX']])
 
 def domain(path):
@@ -15,24 +19,29 @@ def domain(path):
 def static_from_root():
     return send_file('/'.join([app.config['STATIC_FOLDER'], request.path[1:]]))
 
-@mod.route('/', defaults={'path': ''})
+@mod.route('/')
+def index():
+    return render_template(template_path('index.html'))
+
 @mod.route('/<path:path>')
 def root(path):
     try :
         if path.startswith(app.config['COMMON_FOLDER']) :
+            # The path starts with /common, send from the static folder
             return send_file('/'.join([app.config['STATIC_FOLDER'], path]))
         elif '.' in path.split('/')[-1] :
+            # The last path element has an *.something extension
             return send_file(domain(path))
         else :
-            return send_file(index(path))
+            # The path is for a static directory we don't usually support
+            return send_file(static_index(path))
     except IOError:
         abort(404)
 
 @mod.route('/events')
 @mod.route('/events/')
 def events() :
-    template = '/'.join([app.config['HOST'], 'events.html'])
-    return render_template(template)
+    return render_template(template_path('events.html'))
 
 @mod.route('/work')
 def work() :
@@ -47,8 +56,7 @@ def work_individual(category, slug) :
     work = db.work.find_one({'host':app.config['HOST'], 'slug':slug})
     if work is not None :
         work['media'] = deref_list('media', work['media'])
-    template = '/'.join([app.config['HOST'], 'work_individual.html'])
-    return render_template(template, work=work)
+    return render_template(template_path('work_individual.html'), work=work)
 
 def deref_list(collection, _list):
     return [dereference(collection, x) for x in _list]
