913c2d5d3cef56e430880543adabf941929e0e95
Dustin Ingram <dustin.ingram@gmail.com>
2014-06-08 12:03:09 -0400

    Moved image retrieval function to common lib

--- portphilio/views/frontend.py
+++ portphilio/views/frontend.py
@@ -1,9 +1,6 @@
-from flask import Blueprint, request, send_file, abort, render_template, redirect
+from flask import Blueprint, request, send_file, abort, render_template
 from flask import current_app as app
-from portphilio_lib.tools import get_work_from_slug
-import requests
-import boto
-import os
+from portphilio_lib.tools import get_work_from_slug, retrieve_image
 
 mod = Blueprint('frontend', __name__)
 
@@ -80,57 +77,4 @@ def work_individual(category, slug):
 
 @mod.route('/image/<image_name>')
 def image(image_name):
-    size = request.args.to_dict()
-    w = size.get('w', None)
-    h = size.get('h', None)
-    split = image_name.split(".")
-    fn = "".join(split[0:-1])
-    ext = split[-1]
-    params = {}
-    bucket_name = "%s_%s" % (
-        os.environ['S3_BUCKET'], app.config['OWNER'].username)
-    url = "https://%s.s3.amazonaws.com/" % (bucket_name)
-
-    if w is not None or h is not None:  # Some size is given
-        fn += "-"  # Separator for size
-        if w is not None:
-            params["width"] = int(w)
-            fn += "%sw" % (w)
-        if h is not None:
-            params["height"] = int(h)
-            fn += "%sh" % (h)
-
-        conn = boto.connect_s3()
-        bucket = conn.get_bucket('portphilio_maggiecasey')
-        key = bucket.get_key("%s.%s" % (fn, ext))
-
-        if key is None:
-            blit_job = {
-                "application_id": os.environ['BLITLINE_APPLICATION_ID'],
-                "src": {
-                    "name": "s3",
-                    "bucket": bucket_name,
-                    "key": image_name
-                },
-                "functions": [
-                    {
-                        "name": "resize_to_fit",
-                        "params": params,
-                        "save": {
-                            "image_identifier": image_name,
-                            "s3_destination": {
-                                "bucket": bucket_name,
-                                "key": "%s.%s" % (fn, ext)
-                            },
-                        }
-                    }
-                ]
-            }
-            r = requests.post(
-                "http://api.blitline.com/job",
-                data={
-                    'json': json.dumps(blit_job)})
-            app.logger.debug(r.text)
-        return redirect("%s%s.%s" % (url, fn, ext))
-    else:
-        return redirect("%s%s" % (url, image_name))
+    return retrieve_image(image_name, app.config['OWNER'].username)
