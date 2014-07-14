3669d8e92df36f6cbf703570950a0925549fe684
Dustin Ingram <dustin.ingram@gmail.com>
2014-07-14 16:50:44 -0400

    Now using Landlord

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from portphilio import create_app
-from portphilio.host_dispatch import HostDispatcher
+from landlord import Landlord
 from flask import Flask
 
 if __name__ == '__main__' :
     app = Flask(__name__)
     app.debug = os.environ.get('FLASK_DEBUG') == 'True'
-    app.wsgi_app = HostDispatcher(create_app)
+    app.wsgi_app = Landlord(create_app)
     port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port, use_debugger=True, use_reloader=True)
