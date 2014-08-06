1bc178bc632956833080c7a8a1823507202cab2f
Dustin Ingram <dustin.ingram@gmail.com>
2014-08-06 15:44:35 -0400

    Added newrelic reporting

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import os
 from facade import create_app
 from landlord import Landlord
 from flask import Flask
+import newrelic.agent
 
 if __name__ == '__main__' :
     app = Flask(__name__)
     app.debug = os.environ.get('FLASK_DEBUG') == 'True'
     app.wsgi_app = Landlord(create_app)
+    newrelic.agent.initialize('newrelic.ini')
     port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port, use_debugger=True, use_reloader=True)
