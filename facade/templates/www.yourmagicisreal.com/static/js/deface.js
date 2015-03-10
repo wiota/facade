37fdef3209b22add89ee4cc1724d1cbae7c9d4a9
ryanhinkel <ryanhinkel@gmail.com>
2015-03-09 23:16:15 -0400

    Augmented deface library with publish/subscribe methods

--- facade/templates/www.yourmagicisreal.com/static/js/deface.js
+++ facade/templates/www.yourmagicisreal.com/static/js/deface.js
@@ -10,11 +10,33 @@
 
     this[name] = function(){
       var inst = new fn();
+      $.extend(fn.prototype, events);
       inst.init.apply(inst, arguments);
       return inst;
     };
 
     return fn;
   }
+
+  var events = {
+    // The trigger event must be augmented separately because it requires a
+    // new Event to prevent unexpected triggering of a method (and possibly
+    // infinite recursion) when the event type matches the method name
+    trigger: function (type, data) {
+      var event = new $.Event(type);
+      event.preventDefault();
+      $.event.trigger(event, data, this);
+      return this;
+    }
+  };
+
+  // Augment the object with jQuery's event methods
+  $.each(['one', 'on', 'off'], function (i, method) {
+    events[method] = function (type, data, fn) {
+      $(this)[method](type, data, fn);
+      return this;
+    };
+  });
+
 
 })(this);
