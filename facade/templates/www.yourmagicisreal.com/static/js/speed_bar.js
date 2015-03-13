997e1d320e6ca836239c79a78c5df0181f55d996
ryanhinkel <ryanhinkel@gmail.com>
2015-03-12 23:16:11 -0400

    Speed bar added to deface library

--- facade/templates/www.yourmagicisreal.com/static/js/speed_bar.js
+++ facade/templates/www.yourmagicisreal.com/static/js/speed_bar.js
@@ -1,32 +1,41 @@
+(function(){
 
-  // speed set --------------------------------------
+  var speed_bar = DEFACE.create('speed_bar');
 
-  var speed_bar
-  // speed bar indicator
-    this._speed_bar_timer = null;
-    this._speed_message = '';
-    this._speed_units = '';
+  speed_bar.prototype.init = function(trigger, speed_bar, display){
+    this.$trigger = $(trigger);
+    this.$display = $(display);
+    this.$speed_bar = $(speed_bar);
+
+    this._timer;
+    this._stopwatch;
     this._speed_bar_speed = 3;
 
-    //this.setup_interface();
-    this.setup_display();
+    this._add_stopwatch(display)
+    this._setup_interface();
+  }
 
-  setup_interface = function(){
+  speed_bar.prototype._add_stopwatch = function(display){
+    this._stopwatch = DEFACE.stopwatch(display);
+  }
+
+
+  speed_bar.prototype._setup_interface = function(){
     var t = this;
 
-    $(this.trigger).on('touchstart', function(e){
+    $(this.$trigger).on('touchstart', function(e){
       t.pause();
       e.preventDefault();
       return false;
     })
 
-    $(this.trigger).on('touchend', function(e){
+    $(this.$trigger).on('touchend', function(e){
       e.preventDefault();
       t.resume();
       return false;
     })
 
-    $(this.trigger).on('mousedown', function(e){
+    $(this.$trigger).on('mousedown', function(e){
       if(e.which!=1){
         // if not primary mouse button
         return true;
@@ -35,70 +44,47 @@
       t.pause();
       //return false;
     })
-    $(this.trigger).on('mouseup', function(e){
+    $(this.$trigger).on('mouseup', function(e){
       e.preventDefault();
       t.resume();
       //return false;
     })
   },
 
-  setup_display = function(){
-    this._speed_bar = $('.speed_bar');
-    this._speed_bar.css({
-      'display':'block',
-      'position':'absolute',
-      'left':'0',
-      'top':'0',
-      'bottom':'0'
-    })
 
-    this._speed_text = $('.speed_text');
-
-    var css = {
-      'position':'absolute',
-      'top': 0,
-      'left': 0,
-      'right': 0,
-      'z-index':3
-      }
-  },
-
-  remove_speed_bar = function(){
-    clearInterval(this._speed_bar_timer)
-    this._speed_bar.css({'width':'0px'})
-  },
-
-  init_new_timer = function(interval){
-    var t = this;
-
-    t.speed_display(10);
-
-    this._speed_bar_timer = setInterval(function(){
-      var w = t.speed_bar.width()+t.speed_bar_speed;
-      t.speed_bar.css({'width':w+'px'})
-      var seconds = t.speed_display((new Date() - t.timer_start)/t.speed_control_ratio);
-
-      if(seconds >= 3){
-        t.resume();
-      }
-    },10)
-
-  }
-
-  pause = function(){
+  speed_bar.prototype._remove_speed_bar = function(){
     clearInterval(this._timer);
-    this.clean_queue()
-    this._timer_start = new Date();
-    this.init_new_timer();
+    this.$speed_bar.css({'width':'0px'})
+  },
+
+  speed_bar.prototype._add_speed_bar = function(interval){
+    var t = this;
+    var milliseconds = 10;
+    this._timer = setInterval(function(){
+      var add = t._speed_bar_speed;
+      console.log(add);
+      var w = t.$speed_bar.width() + add;
+      t.$speed_bar.css({'width':w+'px'})
+    },milliseconds)
   }
 
-  resume = function(){
-    if(!this._timer_start){
-      return false;
-    }
-    this._timer_end = new Date();
-    this._speed = (this._timer_end - this._timer_start)/this._speed_control_ratio;
-    this._timer_start = null;
-    this.start_cycle();
-    this.remove_speed_bar();
+  speed_bar.prototype.pause = function(){
+    this._stopwatch.reset();
+    this._stopwatch.start();
+    this._add_speed_bar();
+    this.trigger('pause');
   }
+
+  speed_bar.prototype.resume = function(){
+    this._stopwatch.stop();
+    var time_elapsed = this._stopwatch.time_elapsed();
+
+    //
+    this._speed = time_elapsed;
+    console.log(time_elapsed);
+    this._timer_start = false;
+    this._remove_speed_bar();
+    this.trigger('resume', this._speed);
+  }
+
+})()
