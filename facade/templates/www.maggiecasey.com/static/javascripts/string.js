4325fbc491e8c3ada82e3d2ab86c3205f35a3b71
Dustin Ingram <di@users.noreply.github.com>
2015-05-11 19:42:29 -0400

    Call static function of static blueprint

--- facade/templates/www.maggiecasey.com/static/javascripts/string.js
+++ facade/templates/www.maggiecasey.com/static/javascripts/string.js
@@ -2,4 +2,4 @@ var flashvars = {};
 var params = {};
 var attributes = {};
 attributes.id = "string";
-swfobject.embedSWF("{{ url_for('static', filename='flash/string_index_cleaned_name.swf')}}", "string", "1024", "239", "9.0.0", false, flashvars, params, attributes);
+swfobject.embedSWF("{{ url_for('static.static', filename='flash/string_index_cleaned_name.swf')}}", "string", "1024", "239", "9.0.0", false, flashvars, params, attributes);
