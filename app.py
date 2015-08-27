df1d79839cb4af6a01a33d85a86e8d43177e7915
Dustin Ingram <di@users.noreply.github.com>
2015-08-27 13:18:20 -0400

    Use gunicorn

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from landlord import Landlord
 from flask import Flask
 import newrelic.agent
 
-if __name__ == '__main__' :
-    app = Flask(__name__)
-    app.debug = os.environ.get('DEVEL', 'FALSE').upper() == 'TRUE'
-    app.wsgi_app = Landlord(create_app, subdomains=['www', 'static'])
-    newrelic.agent.initialize('newrelic.ini')
+app = Flask(__name__)
+app.debug = os.environ.get('DEVEL', 'FALSE').upper() == 'TRUE'
+app.wsgi_app = Landlord(create_app, subdomains=['www', 'static'])
+newrelic.agent.initialize('newrelic.ini')
+
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=port, use_reloader=True)
