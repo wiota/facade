ed0661154eb91c582dfb2e0c2c7d0f39c9b37d95
ryanhinkel <ryanhinkel@gmail.com>
2016-02-10 00:59:08 -0500

    Action is primary video fix for speed optimizations

--- facade/templates/actionisprimary.com/static/js/imgEnlarge.js
+++ facade/templates/actionisprimary.com/static/js/imgEnlarge.js
@@ -1,60 +1,64 @@
-function renderImg(src, className){
-  return $("<div class='"+className+"'><img src='"+src+"' /></div>")
-}
-
-function renderBackdrop(){
-  return $("<div class='photo_backdrop'></div>")
-}
-
-function mount(node, parent){
-  return node.appendTo(parent);
-}
-
-function toCssUnits(number, unit){
-  unit = unit || 'px'
-  return (
-    parseInt(number)!==0 ?
-      number+unit :
-      "0"
-  );
-}
-
-function place(src, parent, el){
-  var screenRatio = $(window).width() / $(window).height();
-  var imgRatio = el.width() / el.height();
-  var offset = $(window).scrollTop();
-  var backdrop = mount(renderBackdrop(), parent);
-  if(screenRatio > imgRatio){
-    var img = mount(renderImg(src, 'enlarged tall'), parent);
-  } else {
-    var img = mount(renderImg(src, 'enlarged'), parent);
+var imageEnlarger = (function(){
+  function renderImg(src, className){
+    return $("<div class='"+className+"'><img src='"+src+"' /></div>")
   }
 
-  function fn (){
-    backdrop.remove();
-    img.remove();
+  function renderBackdrop(){
+    return $("<div class='photo_backdrop'></div>")
   }
 
-  img.css({
-    position: "absolute",
-    top: toCssUnits(offset)
-  })
-  img.on('click', fn);
-  backdrop.on('click', fn);
-}
+  function mount(node, parent){
+    return node.appendTo(parent);
+  }
 
-function pagePlace(parent, event){
-  var element = $(event.target.parentNode);
-  var parent = $(parent);
-  var src = element.data('large-img');
-  place(src, parent, element)
-}
+  function toCssUnits(number, unit){
+    unit = unit || 'px'
+    return (
+      parseInt(number)!==0 ?
+        number+unit :
+        "0"
+    );
+  }
 
-function pageEnlarge(selector, parent){
-  $(selector).on('click', pagePlace.bind(null, parent));
-}
+  function place(src, parent, el){
+    var screenRatio = $(window).width() / $(window).height();
+    var imgRatio = el.width() / el.height();
+    var offset = $(window).scrollTop();
+    var backdrop = mount(renderBackdrop(), parent);
+    if(screenRatio > imgRatio){
+      var img = mount(renderImg(src, 'enlarged tall'), parent);
+    } else {
+      var img = mount(renderImg(src, 'enlarged'), parent);
+    }
+
+    function fn (){
+      backdrop.remove();
+      img.remove();
+    }
+
+    img.css({
+      position: "absolute",
+      top: toCssUnits(offset)
+    })
+    img.on('click', fn);
+    backdrop.on('click', fn);
+  }
+
+  function pagePlace(parent, event){
+    var element = $(event.target.parentNode);
+    var parent = $(parent);
+    var src = element.data('large-img');
+    place(src, parent, element)
+  }
+
+  function pageEnlarge(selector, parent){
+    $(selector).on('click', pagePlace.bind(null, parent));
+  }
+
+  return pageEnlarge;
+})()
 
 $(document).ready( function () {
-  pageEnlarge('.dance .content .image', 'body')
+  imageEnlarger('.dance .content .image', 'body')
 })
 
---
+++ facade/templates/actionisprimary.com/static/js/vidEnlarge.js
@@ -0,0 +1,71 @@
+var videoEnlarger = (function(){
+  function renderYoutube(src, className){
+    return $("<div class='"+className+"'><iframe src="+src+" width='500' height='500' frameborder='0' allowfullscreen></iframe></div>");
+  }
+
+  function renderBackdrop(){
+    return $("<div class='video_backdrop'></div>")
+  }
+
+  function mount(node, parent){
+    return node.appendTo(parent);
+  }
+
+  function toCssUnits(number, unit){
+    unit = unit || 'px'
+    return (
+      parseInt(number)!==0 ?
+        number+unit :
+        "0"
+    );
+  }
+
+  function placeVid(src, parent, el){
+    var screenRatio = $(window).width() / $(window).height();
+    var imgRatio = 1;
+    var offset = $(window).scrollTop();
+    if(screenRatio > imgRatio){
+      var iframe = mount(renderYoutube(src, 'video enlarged tall'), parent);
+    } else {
+      var iframe = mount(renderYoutube(src, 'video enlarged'), parent);
+    }
+
+    var backdrop = mount(renderBackdrop(), parent);
+
+    function fn (){
+      backdrop.remove();
+      iframe.remove();
+    }
+
+    iframe.css({
+      position: "absolute",
+      top: toCssUnits(offset)
+    })
+    iframe.on('click', fn);
+    backdrop.on('click', fn);
+  }
+
+  function pagePlace(parent, event){
+    var target = $(event.target);
+    if(target.hasClass('youtube')){
+      console.log(target)
+      var element = target;
+    } else {
+      var element = target.parents('.youtube.block');
+    }
+    var parent = $(parent);
+    var src = element.data('embed-src');
+    placeVid(src, parent, element)
+  }
+
+  function pageEnlarge(selector, parent){
+    $(selector).on('click', pagePlace.bind(null, parent));
+  }
+
+  return pageEnlarge;
+})()
+
+$(document).ready( function () {
+  videoEnlarger('.dance .content .youtube', 'body')
+})
+
